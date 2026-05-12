214efbffc9b42317a86357415f3d6b7c5eb5b0eb
2026sergio <mirandasegio550@gmail.com>
2026-05-12 09:39:46 -0600

    style: :art: login design and functional logic with javascript
    login design and functional logic with javascript

---
+++ moduloJAVASCRIPT/login.js
@@ -0,0 +1,20 @@
+const $sumbit = document.getElementById("sumbit")
+const $password = document.getElementById("password")
+const $username = document.getElementById("username")
+const $visible = document.getElementById("visible");
+
+document.addEventListener("change",(e)=>{
+    if(e.target ===$visible){      
+        if($visible.checked === false) $password.type = "password";
+        else $password.type = "text";
+    }
+});
+
+document.addEventListener("click", (e)=>{
+    if(e.target === $sumbit){
+        if($password.value !==""&&$username.value !==""){
+            e.preventDefault();
+            window.location.href = "home.html";
+        }
+    }
+})
