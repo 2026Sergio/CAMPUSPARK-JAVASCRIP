5fe3e6446e15d00de88d975b2b7fcc36dbefedf6
2026sergio <mirandasegio550@gmail.com>
2026-05-12 20:22:47 -0600

    fix: :bug: Repair of login and entry with user email and password.
    Repair of login and entry with user email and password.

--- moduloJAVASCRIPT/login.js
+++ moduloJAVASCRIPT/login.js
@@ -1,20 +1,21 @@
-const $sumbit = document.getElementById("sumbit")
-const $password = document.getElementById("password")
-const $username = document.getElementById("username")
-const $visible = document.getElementById("visible");
+const formulario = document.getElementById('formulario');
 
-document.addEventListener("change",(e)=>{
-    if(e.target ===$visible){      
-        if($visible.checked === false) $password.type = "password";
-        else $password.type = "text";
+const extraerDatos = (e) => {
+    e.preventDefault();
+    let user = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    const USER_CORRECTO = "admin@campusparking.com";
+    const PASS_CORRECTA = "Admin123";
+    if (user === USER_CORRECTO && password === PASS_CORRECTA) {
+        console.log("Acceso concedido");
+        window.location.href = './home.html';
+    } else {
+        alert("Usuario o contraseña incorrectos. Intenta de nuevo.");
+        console.log("Acceso denegado");
     }
+}
+formulario.addEventListener('submit', extraerDatos);
+document.getElementById('visible').addEventListener('change', (e) => {
+    const passInput = document.getElementById('password');
+    passInput.type = e.target.checked ? 'text' : 'password';
 });
-
-document.addEventListener("click", (e)=>{
-    if(e.target === $sumbit){
-        if($password.value !==""&&$username.value !==""){
-            e.preventDefault();
-            window.location.href = "home.html";
-        }
-    }
-})
