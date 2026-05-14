4af7b008f5b9136278a86964ec8fe82d9ed7ef3e
2026sergio <mirandasegio550@gmail.com>
2026-05-13 22:24:22 -0600

    fix: :bug: repair and enable form functionality and save form.
    repair and enable form functionality and save form.

--- moduloJAVASCRIPT/nuevo.js
+++ moduloJAVASCRIPT/nuevo.js
@@ -1,11 +1,22 @@
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu-lateral');
 const overlay = document.getElementById('overlay');
-
-const toggleMenu = () => {
+function toggleMenu(){
     menu.classList.toggle('active');
     overlay.classList.toggle('active');
-};
-
+}
 btnMenu.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
+
+
+const formulario = document.getElementById("formulario1");
+
+const tablaBody = document.getElementById("tabla-body");
+
+let vehiculos =
+JSON.parse(localStorage.getItem("vehiculos"))
+|| [];
+
+mostrarVehiculos();
+
+
