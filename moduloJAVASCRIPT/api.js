7f8f7a52eec8683b00d6774f8e48c7850430a87b
2026sergio <mirandasegio550@gmail.com>
2026-05-16 19:08:41 -0600

    fix: :bug: Final touches and repairs to the API and animations.
    Final touches and repairs to the API and animations.

--- moduloJAVASCRIPT/api.js
+++ moduloJAVASCRIPT/api.js
@@ -1,11 +1,29 @@
-fetch("https://api.open-meteo.com/v1/forecast?latitude=14.6407&longitude=-90.5133&current_weather=true")
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        
-        var climaDiv = document.getElementById("info-clima");
-        
-        if(climaDiv) {
-            climaDiv.innerHTML = "Clima: " + datos.current_weather.temperature + "°C <br>" + 
-                                 "Tiempo: " + datos.current_weather.time;
-        }console.log(datos)
+async function cargarClimaDeGuate() {
+    
+ 
+    var divClima = document.getElementById("info-clima");
+    divClima.innerHTML = "Cargando datos...";
+
+    await new Promise(function(resolve) {
+        setTimeout(resolve, 2000);
     });
+
+    try {
+      
+        var respuesta = await fetch("https://api.open-meteo.com/v1/forecast?latitude=14.6407&longitude=-90.5133&current_weather=true");
+        
+        var datos = await respuesta.json();
+        
+        var temperatura = datos.current_weather.temperature;
+        var hora = datos.current_weather.time;
+
+        divClima.innerHTML = "Temperatura: " + temperatura + "°C <br> Hora: " + hora;
+        
+        console.log("¡Clima cargado con éxito!");
+
+    } catch (error) {
+        console.log("Hubo un error con la API");
+        divClima.innerHTML = "No se pudo cargar el clima";
+    }
+}
+cargarClimaDeGuate();
